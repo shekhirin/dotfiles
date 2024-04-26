a261eb5b1d6817c42ec7c79ee3f479f8b61428be
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-04-26 16:21:42 +0100

    feat(nvim): add llvm to ft

--- config/nvim/lua/config/ft.lua
+++ config/nvim/lua/config/ft.lua
@@ -3,7 +3,10 @@ vim.filetype.add({
   filename = {
     [".env"] = "sh",
     [".envrc"] = "sh",
-    ["*.env"] = "sh",
-    ["*.envrc"] = "sh"
-  }
+  },
+  extension = {
+    env = "sh",
+    envrc = "sh",
+    ll = "llvm",
+  },
 })
