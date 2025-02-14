47fa8c2f54375cee8dc7dae0ae96cedfa9f23142
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-02-14 12:52:44 +0000

    fix(nvim): do not delete first word on autocomplete

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -53,7 +53,7 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete({}),
         ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
+          behavior = cmp.ConfirmBehavior.Insert,
           select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
