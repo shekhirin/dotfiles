0830eb07f10dd623d94e4b0bd231f0c8a47afce6
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-11-14 22:57:20 +0000

    feat(shell): more completions

--- modules/shared/shell.nix
+++ modules/shared/shell.nix
@@ -13,12 +13,14 @@
       extraConfig = ''
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/ack/ack-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/adb/adb-completions.nu *
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/aerospace/aerospace-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/bat/bat-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/cargo/cargo-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/curl/curl-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/fastboot/fastboot-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/gh/gh-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/just/just-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/make/make-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/man/man-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
@@ -31,6 +33,7 @@
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/rustup/rustup-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/tar/tar-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/tcpdump/tcpdump-completions.nu *
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/uv/uv-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/yarn/yarn-v4-completions.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/modules/git/git.nu *
         # Aliases go after git module to take precedence over it
