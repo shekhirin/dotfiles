c5b0d0aa23067b4476664465c70691a4f9732892
Alexey Shekhirin <github@shekhirin.com>
2026-01-17 20:07:26 +0000

    nushell: add DOCKER_HOST for rootless docker on NixOS

--- modules/nixos/shell.nix
+++ modules/nixos/shell.nix
@@ -4,5 +4,8 @@
   # Extend nushell configuration with NixOS-specific config
   programs.nushell = {
     configFile.text = lib.mkAfter ("\n" + builtins.readFile ./config.nu);
+    extraEnv = ''
+      $env.DOCKER_HOST = "unix:///run/user/1000/docker.sock"
+    '';
   };
 }
