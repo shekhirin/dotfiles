15366adc87fba70d1108199ee1a121e87e9048d3
Alexey Shekhirin <github@shekhirin.com>
2026-02-16 11:10:29 +0000

    deps: flake update

--- modules/shared/programs/amp.nix
+++ modules/shared/programs/amp.nix
@@ -4,5 +4,8 @@
   home.file.".config/amp/settings.json".text = builtins.toJSON {
     "amp.dangerouslyAllowAll" = true;
     "amp.experimental.compaction" = 95;
+    "amp.internal.model" = {
+      "deep" = "openai:gpt-5.3-codex-api-preview";
+    };
   };
 }
