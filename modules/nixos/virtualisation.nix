18b2abd0bb181207a95146ff7ae459fe80fbc6b4
Alexey Shekhirin <github@shekhirin.com>
2026-03-02 23:18:41 +0000

    fix(nixos): remove deprecated qemu.ovmf option

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -3,7 +3,6 @@
   virtualisation.libvirtd = {
     enable = true;
     onBoot = "start"; # autostart VMs marked with virsh autostart
-    qemu.ovmf.enable = true;
   };
 
   programs.virt-manager.enable = true;
