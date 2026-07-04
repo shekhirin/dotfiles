09d20090b920218d3cf84b6c3bcf93dd290fe71d
Alexey Shekhirin <github@shekhirin.com>
2026-07-04 20:23:03 +0100

    fix(nixos): limit boot generations

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -21,7 +21,7 @@ in
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
